bf7640cb25d4ac485b6c9ff81ac66a9e8f84fbc8
Netotest <sakamotokenshin@yandex.ru>
2024-02-22 13:32:46 +0300

    Task completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,16 @@
 public class Main {
+
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Иван";
+        post.passport = "4444 № 44444444";
+        post.patronymic = "Иванович";
+        post.phone = "8(999)999-99-99";
+        post.surname = "Иванов";
+        post.subscription = true;
+        post.birthday = new FormDate();
+        post.birthday.day = 13;
+        post.birthday.month = 4;
+        post.birthday.year = 1998;
+    }
 }
